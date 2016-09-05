86f163da00a0c768e1786d6e5b4db2cb5deaa188
richousrick <richousrick@users.noreply.github.com>
2016-09-05 15:51:06 +0100

    Formatted cmd's header

--- Bat/cmd.bat
+++ Bat/cmd.bat
@@ -1,5 +1,8 @@
-::takes input and outputs it to cmd and prints the output to bat stdout
-::useful to access cmd where it has been blocked but command prompt script processing has not
+:: Name: cmd.bat
+:: Author: Richousrick
+:: Description: takes input and outputs it to cmd and prints the output to bat stdout,
+::              useful to access cmd where it has been blocked but command prompt script processing has not
+
 @ECHO OFF
 set exit="exit"
 :beginning
