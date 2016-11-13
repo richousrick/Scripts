8c131879842b27e7af557dc52dfb37f716e3bb60
richousrick <richousrick@users.noreply.github.com>
2016-11-13 17:53:13 +0000

    Added ability to move multiple files

--- Java/FileMover/MoveToGrandparent.java
+++ Java/FileMover/MoveToGrandparent.java
@@ -1,4 +1,7 @@
+package file_mover;
+
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * Moves a file or folder up a directory.<br>
@@ -8,27 +11,41 @@ import java.io.File;
 public class MoveToGrandparent {
 
 	/**
-	 * MoveToParent <file> [y]
+	 * MoveToGrandParent [y] <file's>
 	 * <> required
 	 * [] optional
-	 * [y] - move to parent even if file exists
+	 * [y] - move to grand parent even if file exists
 	 */
 	public static void main(String[] args) {
-		if(args.length<1){
-			System.out.println("MoveToParent <file> [y]\n[y] - optional, \n\tmove to parent even if file exists");
-		}else{
-			File f = new File(args[0]);
-			if(f.exists()){
-				File destination = new File(new File(f.getParent()).getParent()+"/"+f.getName());
-				if(!destination.exists()||(args.length>1&&args[1].equals("y"))){
-					f.renameTo(destination);
-					System.out.println(destination);
-				}else{
-					System.out.println("File with that name exists in the parent\nTo move anyway add parameter y");
+		System.out.println("input: "+Arrays.toString(args));
+		boolean force = false;
+		
+		if(args.length>0){
+			int i = 0;
+			if(args[0].equals("y")){
+				force = true;
+				if(args.length<2){
+					System.err.println("Error : MoveToGrandParent [y] <file's> \n[y] - optional, \n\tmove to grand parent even if file exists");
 				}
-			}else{
-				System.out.println("File does not exist");
+				i=1;
 			}
+			
+			for(; i< args.length; i++){
+				File f = new File(args[i]);
+				if(f.exists()){
+					File destination = new File(new File(f.getParent()).getParent()+"/"+f.getName());
+					if(!destination.exists()||force){
+						f.renameTo(destination);
+						System.out.println(destination);
+					}else{
+						System.err.println("Error: File with the name \""+f.getName()+"\" exists in the grand parent\nTo move anyway add parameter y");
+					}
+				}else{
+					System.err.println("Error: File does not exist");
+				}
+			}
+		}else{	
+			System.err.println("Error : MoveToGrandParent [y] <file's> \n[y] - optional, \n\tmove to grand parent even if file exists");
 		}
 	}
 }
